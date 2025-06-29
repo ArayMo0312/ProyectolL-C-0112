4733b681e27cbaa230cd6c2635a917ae61ad99d1
Jarek <jak3093@gmail.com>
2025-06-28 23:27:17 -0600

    Agregado llamado a VentanaCola

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        
+        ColaMascotas cola = new ColaMascotas();
+        VentanaCola ventana = new VentanaCola(cola);
+        SwingUtilities.invokeLater(() -> new VentanaCola(cola));
+        ventana.setVisible(true);
     }
 }
