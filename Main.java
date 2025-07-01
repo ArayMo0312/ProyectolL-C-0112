7b64ae13af2e0b7766b34167bfd35a9ba731cc3d
Jarek <jak3093@gmail.com>
2025-07-01 10:23:07 -0600

    Agregado actualizarCola al iniciar la GUI

--- Main.java
+++ Main.java
@@ -9,11 +9,13 @@ public class Main {
         arbol.cargarDesdeArchivo("historial.txt");
 
         cola.cargarColaArchivo("cola.txt"); // Llama siempre al método de carga antes de mostrar la interfaz
+
         System.out.println("Ruta actual: " + new java.io.File(".").getAbsolutePath());
         SwingUtilities.invokeLater(() -> {
 
             ClinicaGUI clinica = new ClinicaGUI(cola, arbol);
             clinica.setVisible(true);
+            clinica.actualizarCola();
         });
     }
 }
