38b4a7e3d1dcf658cc534aa2888baad0f19ba421
Jarek <jak3093@gmail.com>
2025-07-03 13:19:55 -0600

    Agregado botón y funcionalidad de 'Encolar' en panelAcciones

--- HistorialMascotasPanel.java
+++ HistorialMascotasPanel.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 
 public class HistorialMascotasPanel extends JPanel {
     private ArbolMascotas arbol;
+    private ColaMascotas cola;
     private JTextArea areaHistorial;
     private JTextField campoBusqueda;
     private JButton botonBuscar;
     private JButton botonEliminar;
+    private JButton botonEncolar;
+    private ClinicaGUI ventanaPadre; 
 
-    public HistorialMascotasPanel(ArbolMascotas arbol) {
+    public HistorialMascotasPanel(ClinicaGUI ventanaPadre, ArbolMascotas arbol, ColaMascotas cola) { // Se le pasa la instancia de ClinicaGUI para poder llamar al método actualizarCola()
         this.arbol = arbol;
+        this.ventanaPadre = ventanaPadre;
+        this.cola = cola;
         setLayout(new BorderLayout());
 
         // Área principal para mostrar historial
@@ -22,12 +27,13 @@ public class HistorialMascotasPanel extends JPanel {
         scroll.setBorder(BorderFactory.createTitledBorder("Historial completo"));
         add(scroll, BorderLayout.CENTER);
 
-        // Panel inferior de acciones (buscar y eliminar por ID)
+        // Panel inferior de acciones (buscar, eliminar y encolar por ID)
 
         JPanel panelAcciones = new JPanel();
         campoBusqueda = new JTextField(10);
         botonBuscar = new JButton("Buscar");
         botonEliminar = new JButton("Eliminar");
+        botonEncolar = new JButton("Encolar");
 
         //Adicion de componentes al panel de acciones
 
@@ -35,16 +41,18 @@ public class HistorialMascotasPanel extends JPanel {
         panelAcciones.add(campoBusqueda);
         panelAcciones.add(botonBuscar);
         panelAcciones.add(botonEliminar);
+        panelAcciones.add(botonEncolar);
         add(panelAcciones, BorderLayout.SOUTH);
 
         // Eventos al pulssar los botones 
         botonBuscar.addActionListener((ActionEvent e) -> buscarMascota());
         botonEliminar.addActionListener((ActionEvent e) -> eliminarMascota());
+        botonEncolar.addActionListener((ActionEvent e) -> encolarMascota());
 
         mostrarHistorial();
     }
 
-    //Metodo para mostrar todas las Mascotas en cola y atedidas recorriendo el arbol
+    //Metodo para mostrar todas las Mascotas en cola y atendidas recorriendo el arbol
 
     private void mostrarHistorial() {
         areaHistorial.setText("");
@@ -84,6 +92,34 @@ public class HistorialMascotasPanel extends JPanel {
         }
         catch(NumberFormatException e){
             JOptionPane.showMessageDialog(this, "El ID debe ser un numero entero.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    // Método para encolar del árbol a la cola
+    private void encolarMascota() {
+        try {
+            int id = Integer.parseInt(campoBusqueda.getText().trim());
+            Mascota m = arbol.buscar(id);
+
+            if (m == null) {
+                JOptionPane.showMessageDialog(this, "Mascota no encontrada en el registro.", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            if (cola.yaExiste(m)) {
+                JOptionPane.showMessageDialog(this, "La mascota ya está en la cola.", "Duplicado", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            cola.enqueue(m);
+            ventanaPadre.actualizarCola();
+            JOptionPane.showMessageDialog(this, "Mascota encolada exitosamente", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            
+            
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "El ID debe ser un número entero.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
